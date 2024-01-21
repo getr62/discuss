59dc230a38250c00e724d672e7fedb21165c13bf
getr62 <georg.triendl@gmail.com>
2024-01-21 15:44:43 +0000

    add useCurrentSession hook

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardPage() {
+  return <div className='flex justify-center mx-auto mt-36'>Dashboard</div>;
+}
--- src/components/header-auth.tsx
+++ src/components/header-auth.tsx
@@ -1,95 +1,26 @@
 'use client';
 
-import Link from 'next/link';
+import * as actions from '@/actions';
+import { useCurrentSession } from '@/hooks/useGetSession';
 import {
+  Button,
   Chip,
   NavbarItem,
-  Button,
   Popover,
-  PopoverTrigger,
   PopoverContent,
+  PopoverTrigger,
 } from '@nextui-org/react';
+import Link from 'next/link';
 import { Icon } from 'react-icons-kit';
 import { pacman } from 'react-icons-kit/icomoon/pacman';
-import { getSession, useSession } from 'next-auth/react';
-import * as actions from '@/actions';
-import { useEffect, useRef, useState } from 'react';
-import { getSessionCookie } from '@/actions/get-cookie';
-import { getSessionData } from '@/actions/get-session';
-import type { Session } from 'next-auth';
 
 export default function HeaderAuth() {
-  // const [loading, setLoading] = useState(true);
-  // const [sessionA, setSessionA] = useState<Session | null>(null);
-  // const [sessionB, setSessionB] = useState<Session | null>(null);
-
-  // const componentIsMounted = useRef(true);
-  // useEffect(() => {
-  //   console.log('useEffect for componentIsMounted run');
-  //   // each useEffect can return a cleanup function
-  //   return () => {
-  //     componentIsMounted.current = false;
-  //   };
-  // }, []);
-
-  const session = useSession();
-  console.log('session from useSession: ', session);
-
-  let userSession: Session | null = null;
-  const getUserSession = async () => {
-    userSession = await getSessionData();
-    console.log('getUserSession: ', userSession);
-  };
-  if (!userSession) {
-    getUserSession();
-  }
-
-  // useEffect(() => {
-  //   const fetchSession = async () => {
-  //     const result = await getSession({ broadcast: false, triggerEvent: false });
-  //     console.log('SESSION A from getSession: ', result);
-  //     setSessionA(result);
-  //   };
-
-  //   if (loading) {
-  //     setLoading(false);
-  //   }
-  //   fetchSession();
-  //   console.log('getSessin A in useEffect: ', sessionA);
-  // }, [session.status]);
-
-  // useEffect(() => {
-  //   const getUserSession = async () => {
-  //     const userSession = await getSessionData();
-  //     console.log('SESSION B from getSessionData action: ', userSession);
-  //     setSessionB(userSession);
-  //   };
-
-  //   if (loading) {
-  //     setLoading(false);
-  //   }
-  //   getUserSession();
-  //   console.log('SESSION B in useEffect: ', sessionB);
-  // }, [session.status]);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      console.log('timeout run');
-      if (userSession && session.status === 'unauthenticated') {
-        window.location.reload();
-      }
-    }, 500);
-    return () => clearTimeout(timer);
-    // if (userSession && session.status === 'unauthenticated') {
-    //   console.log('window.location.reload() run');
-    //   window.location.reload();
-    // }
-  });
+  const { session, status } = useCurrentSession();
 
   let authContent: React.ReactNode;
-  if (session.status === 'loading') {
+  if (status === 'loading') {
     authContent = null;
-  } else if (session.data?.user) {
+  } else if (session && status === 'authenticated') {
     authContent = (
       <Popover placement='left'>
         <PopoverTrigger>
@@ -99,7 +30,7 @@ export default function HeaderAuth() {
             variant='faded'
             color='default'
           >
-            {session.data.user.name}
+            {session!.user!.name}
           </Chip>
         </PopoverTrigger>
         <PopoverContent>
@@ -114,7 +45,6 @@ export default function HeaderAuth() {
   } else {
     authContent = (
       <>
-        {/* <Button onClick={() => window.location.reload()}>Reload</Button> */}
         <NavbarItem>
           <Link href={'/sign-in'}>Sign In</Link>
         </NavbarItem>
