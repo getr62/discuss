f38590e20213235d9c6b3359fb60359b7f2ce960
getr62 <georg.triendl@gmail.com>
2024-01-24 19:35:58 +0000

    implement froala

---
+++ src/components/posts/froala.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+// import 'froala-editor/js/plugins/align.min.js';
+// import 'froala-editor/js/plugins/char_counter.min.js';
+
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+
+export default function Froala() {
+  return <FroalaEditorComponent />;
+}
