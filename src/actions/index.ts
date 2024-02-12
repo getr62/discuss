1c5ede0c083e7c54c8be3a27cc7a3a8b08191a50
getr62 <georg.triendl@gmail.com>
2024-02-12 13:50:53 +0000

    add delete post action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,4 +4,5 @@ export { signOut } from './sign-out';
 export { createComment } from './create-comment';
 export { createPost } from './create-post';
 export { createTopic } from './create-topic';
+export { deletePost } from './delete-post';
 export { search } from './search';
--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
+import { Button, Chip } from '@nextui-org/react';
 import PostShow from '@/components/posts/post-show';
 import PostShowLoading from '@/components/posts/post-show-loading';
 import CommentList from '@/components/comments/comment-list';
 import CommentCreateForm from '@/components/comments/comment-create-form';
 import paths from '@/lib/paths';
+import * as actions from '@/actions';
 import { fetchCommentsByPostId } from '@/db/queries/comments';
 
 interface PostShowPageProps {
@@ -16,12 +18,27 @@ interface PostShowPageProps {
 
 export default async function PostShowPage({ params }: PostShowPageProps) {
   const { slug, postId } = params;
+  const deletePostAction = actions.deletePost.bind(null, { slug, postId });
 
   return (
-    <div className="space-y-3">
-      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
-        {'< '}Back to {slug}
-      </Link>
+    <div className='space-y-3'>
+      <div className='flex'>
+        <Link href={paths.topicShow(slug)}>
+          <Chip>
+            {'< '}Back to {slug}
+          </Chip>
+        </Link>
+        <div className='flex ml-auto space-x-2'>
+          <Button color='success' variant='bordered'>
+            Edit
+          </Button>
+          <form action={deletePostAction}>
+            <Button type='submit' color='danger' variant='flat'>
+              Delete
+            </Button>
+          </form>
+        </div>
+      </div>
       <Suspense fallback={<PostShowLoading />}>
         <PostShow postId={postId} />
       </Suspense>
